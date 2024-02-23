13fad43d2c1b6bfec3e269ab1bf46eec33e75a72
anatolii <bychko4891@gmail.com>
2024-02-23 16:17:20 +0200

    one commit

--- app/about/dynamic.tsx
+++ app/about/dynamic.tsx
@@ -3,7 +3,7 @@
 import { ReactNode, useState } from "react";
 
 export function ClientComponent({ children }: { children: ReactNode }) {
-  const [count, setCounet] = useState(0);
+  const [count, setCounter] = useState(0);
 
   return (
     <div>
@@ -12,14 +12,14 @@ export function ClientComponent({ children }: { children: ReactNode }) {
         Count: {count}
         <button
           onClick={() => {
-            setCounet(count + 1);
+            setCounter(count + 1);
           }}
         >
           Add
         </button>
         <button
           onClick={() => {
-            setCounet(count - 1);
+            setCounter(count - 1);
           }}
         >
           Remove
