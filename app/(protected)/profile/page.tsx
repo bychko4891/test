546cb78310e8eda2ce91c5ca60c55a64c27db31d
anatolii <bychko4891@gmail.com>
2024-03-04 23:06:33 +0200

    one two

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -4,27 +4,14 @@ import {getServerSession} from "next-auth";
 import {authConfig} from "@/configs/auth";
 
 
-export async function ServerComponent() {
-    // console.log("use server");
-
-    return <div>Static HTML</div>;
-}
-
 export default async function UserProfile() {
 
     const session = await getServerSession(authConfig);
-    // const about = await getAboutAPI();
-    // console.log(about?.text);
 
-    // console.log(session?.user || " no user ");
     return (
         <>
             <h1>Profile</h1>
-            {/*<h1>{about?.name}</h1>*/}
-            {/*<div dangerouslySetInnerHTML={{ __html: (about?.text!) || 'test'}} />*/}
-            {/*<ClientComponent>*/}
-            {/*    <ServerComponent/>*/}
-            {/*</ClientComponent>*/}
+
         </>
     );
 }
--- app/about/Button.tsx
+++ app/about/Button.tsx
@@ -1,16 +1,8 @@
-import {AboutContent} from "@/app/about/AboutContent";
+export const Button = () => {
 
-export const Button = ({ onClick }:{onClick:any }) => {
-    // const BurgerIcoClickHandler = () => {
-    //     if (onClick) {
-    //         onClick();
-    //     }
-    // }
     return (
         <>
 
-                {/*<button onClick={BurgerIcoClickHandler}>Send form</button>*/}
-            <AboutContent onClick={onClick} />
 
         </>
     );
