01f30e88cc6a9bb3e23d495ba50266b519cb5deb
Pavlo Karas <thepavlokaras@gmail.com>
2024-03-04 18:26:00 +0200

    refactor: nested if statement

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,67 +1,69 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import {
-    getJwtAccessToken,
-    redirectUnauthorized,
-    regenerateAccessToken, regenerateAllTokens, SuccessAccessTokenRegeneration,
+  getJwtAccessToken,
+  redirectUnauthorized,
+  regenerateAccessToken,
+  regenerateAllTokens,
+  SuccessAccessTokenRegeneration,
 } from "./jwtSessionService/authTokenHandler";
-import {cookies} from "next/headers";
-import {JWT_REFRESH_TOKEN} from "../../CooKiesName";
-import {SetAccessToken} from "./jwtSessionService/SetAccessToken";
-import {SetAllTokens} from "./jwtSessionService/SetAllTokens";
+import { cookies } from "next/headers";
+import { JWT_REFRESH_TOKEN } from "../../CooKiesName";
+import { SetAccessToken } from "./jwtSessionService/SetAccessToken";
+import { SetAllTokens } from "./jwtSessionService/SetAllTokens";
 
-export default async function Layout({children}: { children: ReactNode }) {
+export default async function Layout({ children }: { children: ReactNode }) {
+  const accessTokenCookie = await getJwtAccessToken();
+  const accessTokenCookieIsEmpty = accessTokenCookie === undefined;
 
-    const accessTokenCookie = await getJwtAccessToken();
-    const accessTokenCookieIsEmpty = accessTokenCookie === undefined;
+  if (accessTokenCookie) {
+    return (
+      <>
+        <h1> Layout protected</h1>
+        {children}
+      </>
+    );
+  }
 
-    if (accessTokenCookie === undefined) {
-        const accessToken = await newAccessToken(accessTokenCookieIsEmpty);
-        if (accessToken !== undefined) {
-            return (
-                <>
-                    <h1> Layout protected</h1>
-                    {children}
-                    <SetAccessToken accessToken={accessToken} />
-                </>
-            );
-        } else {
-            const tokens = await newRefreshAndAccessToken();
-            return (
-                <>
-                    <h1> Layout protected</h1>
-                    {children}
-                    <SetAllTokens tokens={tokens} />
-                </>
-            );
-        }
-    } else {
-        return (
-            <>
-                <h1> Layout protected</h1>
-                {children}
-            </>
-        );
-    }
+  const accessToken = await newAccessToken(accessTokenCookieIsEmpty);
+  if (accessToken) {
+    return (
+      <>
+        <h1> Layout protected</h1>
+        {children}
+        <SetAccessToken accessToken={accessToken} />
+      </>
+    );
+  }
+
+  const tokens = await newRefreshAndAccessToken();
+  return (
+    <>
+      <h1> Layout protected</h1>
+      {children}
+      <SetAllTokens tokens={tokens} />
+    </>
+  );
 }
 
-async function newAccessToken(accessTokenIsEmpty: boolean): Promise<string | undefined> {
-
-    if (accessTokenIsEmpty) {
-        const refreshToken = cookies().get(JWT_REFRESH_TOKEN);
-        if (!refreshToken) {
-            throw await redirectUnauthorized();
-        }
-
-        return await regenerateAccessToken(refreshToken.value);
+async function newAccessToken(
+  accessTokenIsEmpty: boolean,
+): Promise<string | undefined> {
+  if (accessTokenIsEmpty) {
+    const refreshToken = cookies().get(JWT_REFRESH_TOKEN);
+    if (!refreshToken) {
+      throw await redirectUnauthorized();
     }
-    return undefined;
+
+    return await regenerateAccessToken(refreshToken.value);
+  }
+  return undefined;
 }
 
 async function newRefreshAndAccessToken(): Promise<SuccessAccessTokenRegeneration> {
-    const refreshToken = cookies().get(JWT_REFRESH_TOKEN);
-    if (!refreshToken) {
-        throw await redirectUnauthorized();
-    }
+  const refreshToken = cookies().get(JWT_REFRESH_TOKEN);
+  if (!refreshToken) {
+    throw await redirectUnauthorized();
+  }
 
-    return await regenerateAllTokens(refreshToken.value);
+  return await regenerateAllTokens(refreshToken.value);
 }
